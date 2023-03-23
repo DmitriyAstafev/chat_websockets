a69ad329f2384c79021c03446386568fe4ea6962
Dmitriy Astafev <dim-as82@yandex.ru>
2023-03-23 17:18:21 +0500

    chat react without redux

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import WebSock from "./components/WebSock";
+
+function App() {
+
+  return (
+      <div>
+        <WebSock/>
+      </div>
+  )
+}
+
+
+export default App;
